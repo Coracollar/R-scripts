904d94956c06a938e0d23bea306b72cfe07ab6f2
Coracollar <57931696+Coracollar@users.noreply.github.com>
2020-05-22 08:44:33 +1000

    Update ImprintedGenes.R

--- ImprintedGenes.R
+++ ImprintedGenes.R
@@ -3,13 +3,13 @@ library(GenomicRanges)
 
 #obtain the list of imprinted genes from file. This one is from the university of san diego. 
 #Is the simplest one, without info if is maternal o paterna. Start and end from UCSC
-imprintedgenes1<-read.table(file="/data/cephfs/punim1048/Methylation_Analysis/genesmethylation/imprintedgenes1.txt", header=T)
+imprintedgenes1<-read.table(file="/Methylation_Analysis/genesmethylation/imprintedgenes1.txt", header=T)
 
 gr_imprintedgenes1<-GRanges(seqnames = imprintedgenes1$mm10.knownGene.chrom,
                             ranges = IRanges(start = imprintedgenes1$mm10.knownGene.txStart, end=imprintedgenes1$mm10.knownGene.txEnd))
 #paternally and maternal expresse genes. From http://www.mousebook.org/imprinting-gene-list?page=4
-paternalexpresedgenes1<-read.table(file="/data/cephfs/punim1048/Methylation_Analysis/genesmethylation/paternalexpressedgenes1.txt", header=T)
-maternalexpresedgenes1<-read.table(file="/data/cephfs/punim1048/Methylation_Analysis/genesmethylation/maternalexpresedgenes1.txt", header=T)
+paternalexpresedgenes1<-read.table(file="/Methylation_Analysis/genesmethylation/paternalexpressedgenes1.txt", header=T)
+maternalexpresedgenes1<-read.table(file="/Methylation_Analysis/genesmethylation/maternalexpresedgenes1.txt", header=T)
 
 creategranges<-function(imprintedgenes){
   GR<-GRanges(seqnames = imprintedgenes$mm10.knownGene.chrom,
