b363b21fd5b79db727b04771ca3d3c749b8acc2a
Coracollar <57931696+Coracollar@users.noreply.github.com>
2020-06-04 17:59:43 +1000

    Update zooming in a region in karyoplotter.R

--- zooming in a region in karyoplotter.R
+++ zooming in a region in karyoplotter.R
@@ -1,1 +1,7 @@
+library(GenomicRanges)
+library(karyoploteR)
 
+zoom.mup <- toGRanges(data.frame("chr4", 59929793, 62169375))
+
+kp <- plotKaryotype("mm10", chromosomes="chr4", zoom=zoom.mup)
+kpDataBackground(kp)
